a93b452fa356a5ebc0b5996d0e14f9ce335f958f
Jakub Trzebiatowski <jaktrzeb@gmail.com>
2025-12-06 12:26:29 +0100

    add String to Item

--- internal/game/domain/types.go
+++ internal/game/domain/types.go
@@ -1,5 +1,7 @@
 package domain
 
+import "fmt"
+
 type Position struct {
 	X uint16
 	Y uint16
@@ -10,6 +12,15 @@ type Item struct {
 	ID       uint16
 	Count    uint8 // Used for stack count, fluid type, or rune charges
 	HasCount bool  // Helper to know if we should write the Count byte
+}
+
+func (i Item) String() string {
+	if i.HasCount || i.Count > 1 {
+		return fmt.Sprintf("ID: %d (x%d)", i.ID, i.Count)
+	}
+
+	// 2. Simple items
+	return fmt.Sprintf("ID: %d", i.ID)
 }
 
 type Tile struct {
